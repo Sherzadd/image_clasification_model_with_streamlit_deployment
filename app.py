2fdadb09b64faa4df2c0ae948174f00b3ccfdf5d
Sherzadoo <sherzadzabihullahg@gmail.com>
2026-01-11 12:42:27 +0100

    icon and email updte

--- app.py
+++ app.py
@@ -259,7 +259,7 @@ with right:
     st.markdown(
         """
 <div style="display:flex; align-items:flex-start; gap:0.75rem;">
-  <div style="font-size:2.6rem; line-height:1;">🌿</div>
+  <div style="font-size:2.6rem; line-height:1;"></div>
   <div style="font-size:2.6rem; font-weight:700; line-height:1.08;">
     Plant Disease identification with AI 🌿
   </div>
@@ -291,7 +291,7 @@ with right:
         st.info(
             "Upload photos and get the result.\n"
             "For best results, follow the User Manual on the left.\n"
-            "For any issues, please contact the app owner."
+            "For any issues, please contact the app owner at .sherzadzabihullah@yahoo.com"
         )
         st.stop()
 
