ca110b3253a5a505cfaba216ce3facfa24d7d1cb
Sherzadoo <sherzadzabihullahg@gmail.com>
2026-01-21 23:58:34 +0100

    new model

--- streamlit_app/tomato/tomatoe_app.py
+++ streamlit_app/tomato/tomatoe_app.py
@@ -58,7 +58,7 @@ div[data-testid="stFileUploader"] button::after {
 # -----------------------------
 BASE_DIR = Path(__file__).resolve().parent
 
-MODEL_PATH = (BASE_DIR / "01_tomato_disease_model_linux.keras").resolve()
+MODEL_PATH = (BASE_DIR / "01_tomato_disease_EfficientNetB0_model_linux").resolve()
 CLASSES_PATH = (BASE_DIR / "tomato_class_names.json").resolve()
 
 
